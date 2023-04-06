81526ba8a7e32aef05e360de3ff2a1c4941c6b05
mohbaba <babamu09@gmail.com>
2023-04-07 00:27:32 +0100

    Poll Unit Results display

--- poll_unit_result/views.py
+++ poll_unit_result/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render , redirect
+from poll_unit_result.models import *
 from django.contrib import messages
 from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
+    if request.method == 'GET':
+        results = request['q']
+        
+        if results:
+            final_results = AnnouncedPuResults.objects.filter(name__icontains = results)
+        
+        else:
+            final_results = AnnouncedPuResults.objects.all()
+    pu_results = AnnouncedPuResults.objects.all()
     return render(request, 'poll_unit_result.html')
